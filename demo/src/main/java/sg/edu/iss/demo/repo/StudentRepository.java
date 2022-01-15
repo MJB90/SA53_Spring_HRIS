5fe71f2fd7b05d63572e914a3fd27279fa57c591
Manasjyoti Borah <35186704+MJB90@users.noreply.github.com>
2022-01-15 17:31:09 +0800

    Delete demo directory

--- demo/src/main/java/sg/edu/iss/demo/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.iss.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.edu.iss.demo.domain.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
